dffb2d2f2b6439e62129ac0959bef0e0566cad18
grospitz-wreck-it <grospitz@gmail.com>
2026-03-17 13:33:34 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import * as L from "leaflet";
+import "./styles.css";
+
+const map = L.map("map").setView([52.114, 8.673], 11);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: "&copy; OpenStreetMap"
+}).addTo(map);
+
+L.marker([52.114, 8.673])
+  .addTo(map)
+  .bindPopup("Herford")
+  .openPopup();
